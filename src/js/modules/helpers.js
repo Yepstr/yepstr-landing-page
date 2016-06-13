9226bdd2a1f38f3f62eb93603f9ab4cad0c4ab1c
Karl Löfholm <karllofholm@Karls-MBP.lan>
2016-06-13 17:25:46 +0200

    fixed github comments

--- src/js/modules/helpers.js
+++ src/js/modules/helpers.js
@@ -18,27 +18,6 @@ var Helpers = {
 
     return data;
   },
-  googleAnalytics: {
-    playVideo: function(pageUrl) { // url: current page url
-      this.gaSendEvent('Video', 'Play', pageUrl, '');
-    },
-    outboundLinks: function(targetUrl) { // label: link label
-      this.gaSendEvent('Outbound Link', 'click', targetUrl, '');
-    },
-    contact: function(type) { // type: mail/phone/etc
-      this.gaSendEvent('Contact footer link', 'click', type, '');
-    },
-    scrollToLink: function(label) { // label: link label
-      this.gaSendEvent('Scroll to link', 'click', label, '');
-    },
-    faqLink: function(label) { // label: link label
-      this.gaSendEvent('FAQ link', 'click', label, '');
-    },
-    // Private functions
-    gaSendEvent: function(eventCategory, eventAction, eventLabel, eventValue) {
-      ga('send', 'event', eventCategory, eventAction, eventLabel, eventValue);
-    },
-  },
 };
 
 module.exports = Helpers;
