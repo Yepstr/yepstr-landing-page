d7e6bf83dd20a9aef3f67aafc72f53f15f9ba0f2
Karl Löfholm <karllofholm@Karls-MacBook-Pro.local>
2016-10-26 11:12:34 +0200

    Some minor fixes

--- src/js/modules/popupVideo.js
+++ src/js/modules/popupVideo.js
@@ -1,11 +1,12 @@
 var $ = require('jquery');
+var globals = require('modules/globals');
 
 var PopupVideo = {
   init: function() {
     var $popupVideoButton = $('.js-popup-video-button');
     if ($popupVideoButton.length == 0) return;
 
-    var isMobile = $(window).width() < app.breakpoints.medium;
+    var isMobile = $(window).width() < globals.breakpoints.medium;
     var $backgroundVideo = $('.js-full-screen-video');
 
     var beforeOpen = function() {
