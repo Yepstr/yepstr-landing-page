997a0753e558fd62632f1e8cbf0f73ad388bdc32
Karl Löfholm <karllofholm@Karls-MacBook-Pro.local>
2016-12-01 16:29:00 +0100

    removed manual GA tracking

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,6 @@ require('vendor/magnificPopup.js');
 
 var scrollToTarget = require('modules/scrollToTarget');
 var scrollToNav = require('modules/scrollToNav');
-var googleAnalyticsEventTracking = require('modules/googleAnalyticsEventTracking');
 var popupVideo = require('modules/popupVideo');
 var activePageHighlight = require('modules/activePageHighlight');
 var imageZoom = require('modules/imageZoom');
@@ -15,7 +14,6 @@ var introMap = require('modules/introMap');
 
 var app = {
   init: function() {
-    googleAnalyticsEventTracking.init();
     scrollToNav.init();
     scrollToTarget.init();
     popupVideo.init();
