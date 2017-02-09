977cb949fbc1cba18f9dac781b63faf8564f5645
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2017-02-09 15:02:19 +0100

    opens up kind of nice modal

--- src/js/modules/createYepSearchSignupFlow.js
+++ src/js/modules/createYepSearchSignupFlow.js
@@ -1,24 +1,49 @@
 var $ = require('jquery');
 var globals = require('modules/globals');
 
-var CREATE_YEP_SEARCH_SIGNUP_FLOW_PATH = 'http://localhost:3000/new-user-create-yep-search/start';
+var CREATE_YEP_SEARCH_SIGNUP_FLOW_PATH = 'https://app.yepstr.com/new-user-create-yep-search/start';
+// var CREATE_YEP_SEARCH_SIGNUP_FLOW_PATH = 'https://yepstr-christmas.herokuapp.com/new-user-create-yep-search/start';
 
 var openModal = function() {
   $.magnificPopup.open({
     items: {
       type: 'inline',
       src: $('<div class="create-task-modal" id="create-task-modal"><iframe src="' + CREATE_YEP_SEARCH_SIGNUP_FLOW_PATH + '" frameborder="0"></iframe></div>')
-    }
+    },
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    showCloseBtn: true,
+    closeOnBgClick: false,
+
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
   });
 }
 var redirect = function() {
   window.location.href = CREATE_YEP_SEARCH_SIGNUP_FLOW_PATH;
-}
+};
+
+var TRUSTED_POSTMAN_ORIGINS = [
+  'http://localhost:3000',
+  'http://localhost:3003',
+  'https://app.yepstr.com',
+  'https://yepstr-christmas.herokuapp.com'
+];
+var receivedPostMessage = function(event) {
+  if (TRUSTED_POSTMAN_ORIGINS.indexOf(event.origin) !== -1) {
+    if (event.data && event.data.redirectLocation) {
+      window.location.href = event.origin + event.data.redirectLocation;
+    }
+  }
+};
 
 var CreateYepSearchSignupFlow = {
   init: function() {
     console.log('ininit');
-
 
     // init button click
     $('.js-modal-toggle').click(function() {
@@ -29,11 +54,9 @@ var CreateYepSearchSignupFlow = {
       } else {
         openModal();
       }
-
-      console.log('cliickity');
     });
-
     // init postman listening
+    window.addEventListener('message', receivedPostMessage, false);
   }
 };
 
